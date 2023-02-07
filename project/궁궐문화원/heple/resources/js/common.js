9322d237abb88e37eae8347366acac93d4b595e1
kimjs <kimjeongs03@gmail.com>
2023-02-07 14:25:23 +0900

    p3

---
+++ project/궁궐문화원/heple/resources/js/common.js
@@ -0,0 +1,49 @@
+$(window).ready(function(){
+	// file upload
+	var fileTarget = $('.filebox .upload-hidden');
+
+    fileTarget.on('change', function(){
+        if(window.FileReader){
+            var filename = $(this)[0].files[0].name;
+        } else {
+            var filename = $(this).val().split('/').pop().split('\\').pop();
+        }
+
+        $(this).siblings('.upload-name').val(filename);
+    });
+
+	//gnb
+	$('.gnb_menu > li').on('click' , function(){
+		$('.gnb_menu > li').removeClass('active');
+		$('#header').removeClass('open-menu');
+		$(this).parents('#header').addClass('open-menu');
+	});
+
+	$('#header').on('mouseleave' , function(){
+		$(this).removeClass('open-menu');
+	});
+
+	/* rating */
+	 if ($('.ip-rating').length > 0) {
+		$('.ip-rating').rating({
+			language: 'ko',
+			showCaption: false,
+			showClear: false,
+			min: 0,
+			max: 5,
+			step: 1,
+		});
+	}
+
+	/* 찜하기 */
+	if ($('.like-badge').length > 0) {
+		$('.like-badge').on('click', function (event) {
+			event.preventDefault(); 
+			var hiddenText;
+
+			$(this).toggleClass('on');
+			hiddenText = $(this).hasClass('on') ? '찜하기' : '찜하기 제거';
+			$(this).text(hiddenText);
+		});
+	}
+});
